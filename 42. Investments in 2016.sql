e5a96805f346a3a223603e90c7f95324e156c27e
R DHANESH <103875277+Dhanesh2210@users.noreply.github.com>
2024-03-12 21:00:54 +0530

    Create 42. Investments in 2016.sql

---
+++ 42. Investments in 2016.sql
@@ -0,0 +1,12 @@
+SELECT ROUND(sum(TIV_2016),2) as tiv_2016 
+FROM insurance
+WHERE TIV_2015 IN
+  (SELECT TIV_2015 
+  FROM insurance
+  GROUP BY 1
+  HAVING COUNT(*) > 1)
+  AND CONCAT(LAT,LON)
+  IN (SELECT CONCAT(LAT,LON)
+      FROM insurance 
+      GROUP BY LAT,LON
+      HAVING COUNT(*) = 1)
