52c0c6d0bed3f7978b34afba266f798e3b896655
R DHANESH <103875277+Dhanesh2210@users.noreply.github.com>
2024-03-12 17:32:49 +0530

    Create 12. Students and Examinations.sql

---
+++ 12. Students and Examinations.sql
@@ -0,0 +1,68 @@
+/*
+Write a solution to find the number of times each student attended each exam.
+Return the result table ordered by student_id and subject_name.
+The result format is in the following example.
+
+Sample Input: 
+Students table:
++------------+--------------+
+| student_id | student_name |
++------------+--------------+
+| 1          | Alice        |
+| 2          | Bob          |
+| 13         | John         |
+| 6          | Alex         |
++------------+--------------+
+Subjects table:
++--------------+
+| subject_name |
++--------------+
+| Math         |
+| Physics      |
+| Programming  |
++--------------+
+Examinations table:
++------------+--------------+
+| student_id | subject_name |
++------------+--------------+
+| 1          | Math         |
+| 1          | Physics      |
+| 1          | Programming  |
+| 2          | Programming  |
+| 1          | Physics      |
+| 1          | Math         |
+| 13         | Math         |
+| 13         | Programming  |
+| 13         | Physics      |
+| 2          | Math         |
+| 1          | Math         |
++------------+--------------+
+Sample Output: 
++------------+--------------+--------------+----------------+
+| student_id | student_name | subject_name | attended_exams |
++------------+--------------+--------------+----------------+
+| 1          | Alice        | Math         | 3              |
+| 1          | Alice        | Physics      | 2              |
+| 1          | Alice        | Programming  | 1              |
+| 2          | Bob          | Math         | 1              |
+| 2          | Bob          | Physics      | 0              |
+| 2          | Bob          | Programming  | 1              |
+| 6          | Alex         | Math         | 0              |
+| 6          | Alex         | Physics      | 0              |
+| 6          | Alex         | Programming  | 0              |
+| 13         | John         | Math         | 1              |
+| 13         | John         | Physics      | 1              |
+| 13         | John         | Programming  | 1              |
++------------+--------------+--------------+----------------+*/
+
+SELECT stu.student_id,
+        stu.student_name,
+        sub.subject_name,   
+        COUNT(e.student_id) AS attended_exams
+FROM Students stu
+CROSS JOIN Subjects sub 
+LEFT JOIN Examinations e
+  ON stu.student_id   = e.student_id   
+    AND sub.subject_name = e.subject_name 
+GROUP BY stu.student_id, stu.student_name, sub.subject_name
+ORDER BY stu.student_id, sub.subject_name
